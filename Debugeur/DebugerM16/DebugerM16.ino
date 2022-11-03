5c964d53cea3e819b29e47c88c1cff1081533181
bbassac <b.bassac@gmail.com>
2022-11-03 14:31:50 +0100

    first release

--- Debugeur/DebugerM16/DebugerM16.ino
+++ Debugeur/DebugerM16/DebugerM16.ino
@@ -4,11 +4,16 @@
 
 
 int pinToTest = 35;
-
-void setup() {
-  //Serial.begin (31250); // attention vitesse de transmission MIDI = 31250, Moniteur Série = 9600
-  Serial.begin (9600);
+int analogIn = A0;
+int velocity = 100;
+//velocity of MIDI notes, must be between 0 and 127
+ //higher velocity usually makes MIDI instruments louder
  
+ int noteON = 144;//144 = 10010000 in binary, note on command
+ int noteOFF = 128;//128 = 10000000 in binary, note off command
+void setup() {
+  //Serial.begin (9600);
+  Serial.begin(31250);
   //Navigation
   pinMode (pinToTest, INPUT_PULLUP);
   pinMode(13, OUTPUT);
@@ -16,6 +21,22 @@ void setup() {
 
 void loop() {
 
+   for (int note=50;note<70;note++) {//from note 50 (D3) to note 69 (A4)
+    MIDImessage(noteON, note, velocity);//turn note on
+    delay(300);//hold note for 300ms
+    MIDImessage(noteOFF, note, velocity);//turn note off
+    delay(200);//wait 200ms until triggering next note
+  }
+
+  testPins();
+}
+void MIDImessage(int command, int MIDInote, int MIDIvelocity) {
+  Serial.write(command);//send note on or note off command 
+  Serial.write(MIDInote);//send pitch data
+  Serial.write(MIDIvelocity);//send velocity data
+}
+
+void testPins(){
     int value = digitalRead (pinToTest); // lecture des boutons
    
     if (value == HIGH) {
@@ -27,6 +48,9 @@ void loop() {
     digitalWrite(13, HIGH);
 
   }
+
+  int tmp = analogRead(analogIn);
+  Serial.println(tmp);
 }
 
 
--- Version16/Version16.ino
+++ Version16/Version16.ino
@@ -12,11 +12,12 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 // Nombre d'entrées analogiques:
-int const Ent_Analog = 1;
+int const nb_fad = 8;
+int const nb_pot = 8;
 // nombre de boutons
 int const Ent_Numerique = 16;
 //sensibilité: 2, ça a l'air bien
-int sensibilite = 5;
+int sensibilite = 10;
 
 
 // déclaration des variables pour les boutons
@@ -60,14 +61,15 @@ boolean prev_button_value[16][2] = { 0, 0,
 // déclaration des variables pour les potentiomètres
 
 // définition broches potentiomètres faders (de droite a gauche)
-int const pot[16] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15 };
-
+int const fad[8] = { A0, A1, A2, A3, A4, A5, A6, A7 };
+int const pot[8] = {A8, A9, A10, A11, A12, A13, A14, A15};
 // attribution control change Midi
-int Cc[16] = { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F };
+int Cc_pot[8] = { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+int Cc_fad[8] = { 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F };
 
 // valeur initiale
-int prev_pot_value[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
+int prev_pot_value[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int prev_fad_value[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
 byte midiChannel = 0;  // canal 1
 int LEFT = 37;
 int UP = 35;
@@ -79,8 +81,8 @@ int prev_nav_right = HIGH;
 int prev_nav_down = HIGH;
 
 void setup() {
-  //Serial.begin (31250); // attention vitesse de transmission MIDI = 31250, Moniteur Série = 9600
-  Serial.begin(9600);
+  Serial.begin (31250); // attention vitesse de transmission MIDI = 31250, Moniteur Série = 9600
+  //Serial.begin(9600);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -102,25 +104,24 @@ void setup() {
 }
 
 void loop() {
-  // Les boutons
-  for (int i = 0; i < Ent_Numerique; i++) {
-    prev_button_value[i][0] = digitalRead(bouton[i][0]);  // lecture des boutons
-    // detection des changements d'état des boutons
-    if (!prev_button_value[i][0] && prev_button_value[i][1]) {  //
-      sendMessage(0x90, bouton[i][1], 127);                     // envoie noteON
-    }
-    if (prev_button_value[i][0] && !prev_button_value[i][1]) {
-      sendMessage(0x80, bouton[i][1], 127);  // envoie noteOFF
-    }
-
-    prev_button_value[i][1] = prev_button_value[i][0];
-  }
-
-  // Les potentiomètres
-  /* for (int j = 0; j < Ent_Analog ; j++) {
-    prev_pot_value [j] = rafraichir (pot[j], prev_pot_value[j], Cc[j]);
+  
+  manageDigitalButtons();
+  manageNavigation();
+  /*
+  // Les faders
+   for (int j = 0; j < nb_fad ; j++) {
+    prev_fad_value [j] = rafraichir (fad[j], prev_fad_value[j], Cc_fad[j]);
   }*/
 
+  // Les potentiometres
+   for (int j = 0; j < nb_pot ; j++) {
+    prev_pot_value [j] = rafraichir (pot[j], prev_pot_value[j], Cc_pot[j]);
+  }
+
+}
+
+void manageNavigation(){
+  
   //Navigation LEFT
   if (prev_nav_left==HIGH && digitalRead(LEFT)== LOW) {
     Serial.println("LEFT appuyé");
@@ -157,6 +158,21 @@ void loop() {
   } 
 }
 
+void manageDigitalButtons(){
+  // Les boutons
+  for (int i = 0; i < Ent_Numerique; i++) {
+    prev_button_value[i][0] = digitalRead(bouton[i][0]);  // lecture des boutons
+    // detection des changements d'état des boutons
+    if (!prev_button_value[i][0] && prev_button_value[i][1]) {  //
+      sendMessage(0x90, bouton[i][1], 127);                     // envoie noteON
+    }
+    if (prev_button_value[i][0] && !prev_button_value[i][1]) {
+      sendMessage(0x80, bouton[i][1], 127);  // envoie noteOFF
+    }
+
+    prev_button_value[i][1] = prev_button_value[i][0];
+  }
+}
 
 int rafraichir(int const pot, int potVal, int Cc) {
   int tmp = analogRead(pot);
