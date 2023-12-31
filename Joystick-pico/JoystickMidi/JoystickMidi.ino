dd9b374bb9a361509aeb4bfd51490a92afdeb26a
b.bassac <b.bassac@lectra.com>
2023-12-31 16:41:59 +0100

    Update JoystickMidi.ino

--- Joystick-pico/JoystickMidi/JoystickMidi.ino
+++ Joystick-pico/JoystickMidi/JoystickMidi.ino
@@ -1,32 +1,38 @@
 #include <MIDI.h>
 
-const int joystickXPin = A1;  // Connect the X-axis of the joystick to analog pin A0
-const int joystickYPin = A0;  // Connect the Y-axis of the joystick to analog pin A1
-const int PINJOYSTICK_BUTTON = 22;
+const int PIN_JOY_X = A1;  // Connect the X-axis of the joystick to analog pin A0
+const int PIN_JOY_Y = A0;  // Connect the Y-axis of the joystick to analog pin A1
+const int PIN_JOY_BUTTON = 22;
 const int PIN_GREEN_LED = 21;
+const int PIN_POWER_LED = 18;
+
 int prev_xValue = 0;
 int prev_yMidivalue = 64;
-int seuil = 3;
-int seuilPitch = 15;
-const int pitchBendRange = 8192;  // MIDI pitch bend range is 0 to 16383, so half is 8192
-
 int prevButtonValue = HIGH;
+
+const int THRESHOLD = 3;
+const int THRESHOLD_PITCH = 15;
+const int PITCH_BEND_RANGE = 8192;  // MIDI pitch bend range is 0 to 16383, so half is 8192
+
+
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   Serial.begin(9600);
-  pinMode(PINJOYSTICK_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_JOY_BUTTON, INPUT_PULLUP);
   pinMode(PIN_GREEN_LED, OUTPUT);
+  pinMode(PIN_POWER_LED, OUTPUT);
+  digitalWrite(PIN_POWER_LED, HIGH);
 }
 
 void loop() {
-  int xValue = analogRead(joystickXPin);
-  int yMidiValue = map(analogRead(joystickYPin) / 8, 0, 127, 127, 0);  // Convert 0->1024 to 0-> 127 ; and invert 127->0
-  int buttonValue = digitalRead(PINJOYSTICK_BUTTON);
+  int xValue = analogRead(PIN_JOY_X);
+  int yMidiValue = map(analogRead(PIN_JOY_Y) / 8, 0, 127, 127, 0);  // Convert 0->1024 to 0-> 127 ; and invert 127->0
+  int buttonValue = digitalRead(PIN_JOY_BUTTON);
 
-  if (abs(xValue - prev_xValue) > seuilPitch) {
-    int pitchBendValue = map(xValue, 0, 1023, -pitchBendRange, pitchBendRange);  //remap  pitchbend
+  if (abs(xValue - prev_xValue) > THRESHOLD_PITCH) {
+    int pitchBendValue = map(xValue, 0, 1023, -PITCH_BEND_RANGE, PITCH_BEND_RANGE);  //remap  pitchbend
     prev_xValue = xValue;
 
     // Send MIDI messages
@@ -36,7 +42,7 @@ void loop() {
     Serial.println(pitchBendValue);
   }
 
-  if (abs(yMidiValue - prev_yMidivalue) > seuil) {
+  if (abs(yMidiValue - prev_yMidivalue) > THRESHOLD) {
     prev_yMidivalue = yMidiValue;
     Serial.print(" Y :");
     Serial.println(yMidiValue);  // Most significant bits of pitch bend
